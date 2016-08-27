98e1242489166538b4761728e35d5eb2e0df6507
timcornwell <realtimcornwell@gmail.com>
2016-08-27 08:19:52 +0100

    Fix imaging.py import error
    fourier_transforms.py

--- arl/pipelines.py
+++ arl/pipelines.py
@@ -3,6 +3,7 @@
 #
 
 from arl.visibility_calibration import solve_gains, qa_gaintable, create_visibility
+from arl.fourier_transforms import predict_visibility, invert_visibility, combine_visibility
 
 def RCAL(parameters, logfile=None):
     """ Real-time calibration
@@ -17,7 +18,40 @@ def RCAL(parameters, logfile=None):
     if qa:
         print("pipelines.RCAL: Solution good")
     return qa
-            
+
+
+def continuum_imaging(parameters, logfile=None):
+    
+    kwargs=parameters['continuum_imaging']
+    
+    nmajor = kwargs.get('nmajor', 5)
+    deconvolver=kwargs.get('deconvolver')
+    sm = kwargs.get("skymodel")
+    
+    print("pipelines.continuum_imaging: Performing %d major cycles" % nmajor)
+    
+    # The model is added to each major cycle and then the visibilities are
+    # calculated from the full model
+    vtpred = predict_visibility(vt, sm, **kwargs)
+    vtres = combine_visibility(vt, vtpred, 1.0, -1.0)
+    dirty, psf, sumwt = invert_visibility(vtres, **kwargs)
+    thresh = kwargs.get("threshold", 0.0)
+    
+    comp = sm.images[0]
+    for i in range(nmajor):
+        print("pipelines.continuum_imaging.solve_skymodel: Start of major cycle %d" % i)
+        cc, res = deconvolver(dirty, psf, **kwargs)
+        comp += cc
+        vtpred = predict_visibility(vt, sm, **kwargs)
+        vtres = combine_visibility(vt, vtpred, 1.0, -1.0)
+        dirty, psf, sumwt = invert_visibility(vtres, **kwargs)
+        if numpy.abs(dirty.data).max() < 1.1 * thresh:
+            print("pipelines.continuum_imaging.solve_skymodel: Reached stopping threshold %.6f Jy" % thresh)
+            break
+        print("pipelines.continuum_imaging.solve_skymodel: End of major cycle")
+    print("pipelines.continuum_imaging.solve_skymodel: End of major cycles")
+    return vtres, sm
+
 if __name__ == '__main__':
     import numpy
     from arl.test_support import create_named_configuration
