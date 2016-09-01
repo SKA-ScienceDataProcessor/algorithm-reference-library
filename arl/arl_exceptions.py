291c465d26fcc882303f59161cb5db4cc819add0
timcornwell <realtimcornwell@gmail.com>
2016-09-01 12:04:25 +0100

    Fix notebook for recent changes
    SHould remember to extend search to *.ipynb

--- arl/arl_exceptions.py
+++
@@ -1,9 +0,0 @@
-"""Exceptions for ARL
-
-realtimcornwell@gmail.com
-"""
-
-class ParameterMissing(BaseException):
-    
-    def __init__(self, funcname):
-        Exception.__init__(self, "Parameters not found for %s" % funcname)
