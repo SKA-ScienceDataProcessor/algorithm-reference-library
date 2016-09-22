0b7e0c9c67c3a906fef42784725360b74110e3db
Peter Wortmann <pw410@cam.ac.uk>
2016-09-22 13:50:17 +0100

    Phase rotation, second pass
    After removing all the weirdness it actually turns out to be fairly
    straightforward. Added some more documentation while I'm at it, and
    make phase rotation pure.
    
    However, create_visibility seems to be behaving weirdly?

--- tests/test_visibility_operations.py
+++ tests/test_visibility_operations.py
@@ -23,26 +23,29 @@ class TestVisibilityOperations(unittest.TestCase):
     def setUp(self):
         self.params = {'wstep': 10.0, 'npixel': 512, 'cellsize':0.0002, 'spectral_mode':'channel'}
 
-        vlaa = filter_configuration(create_named_configuration('VLAA'), self.params)
-        vlaa.data['xyz'] *= 1.0 / 30.0
-        times = numpy.arange(-3.0, +3.0, 6.0 / 60.0) * numpy.pi / 12.0
-        frequency = numpy.arange(1.0e8, 1.50e8, 2.0e7)
+        self.vlaa = filter_configuration(create_named_configuration('VLAA'), self.params)
+        self.vlaa.data['xyz'] *= 1.0 / 30.0
+        self.times = numpy.arange(-3.0, +3.0, 6.0 / 60.0) * numpy.pi / 12.0
+        self.frequency = numpy.arange(1.0e8, 1.50e8, 2.0e7)
 
         # Define the component and give it some spectral behaviour
         f=numpy.array([100.0, 20.0, -10.0, 1.0])
         self.flux = numpy.array([f,0.8*f,0.6*f])
+
         # The phase centre is absolute and the component is specified relative (for now).
         # This means that the component should end up at the position phasecentre+compredirection
         self.phasecentre      = SkyCoord(ra=+15.0*u.deg, dec=+35.0*u.deg, frame='icrs', equinox=2000.0)
-        self.compabsdirection = SkyCoord(ra=17.0*u.deg,  dec=+36.5*u.deg, frame='icrs', equinox=2000.0)
+        self.compabsdirection = SkyCoord(ra=+17.0*u.deg, dec=+36.5*u.deg, frame='icrs', equinox=2000.0)
         # TODO: convert entire mechanism to absolute coordinates
         pcof=self.phasecentre.skyoffset_frame()
         self.compreldirection = self.compabsdirection.transform_to(pcof)
-        self.m31comp = create_skycomponent(flux=self.flux, frequency=frequency, direction=self.compreldirection)
+        self.m31comp = create_skycomponent(flux=self.flux,
+                                           frequency=self.frequency, direction=self.compreldirection)
         self.m31sm = create_skymodel_from_component(self.m31comp)
 
-        vispred = create_visibility(vlaa, times, frequency, weight=1.0, phasecentre=self.phasecentre,
-                                   params=self.params)
+        vispred = create_visibility(self.vlaa, self.times, self.frequency,
+                                    weight=1.0, phasecentre=self.phasecentre,
+                                    params=self.params)
         self.vismodel = predict_visibility(vispred, self.m31sm, self.params)
 
 
@@ -51,14 +54,34 @@ class TestVisibilityOperations(unittest.TestCase):
         summedflux, weight = sum_visibility(self.vismodel, self.compreldirection)
         assert_allclose(self.flux, summedflux , rtol=1e-7)
 
+    def test_phase_rotation_identity(self):
+
+        for newphasecentre in [ SkyCoord(17, 35, unit=u.deg), SkyCoord(17, 30, unit=u.deg),
+                                SkyCoord(12, 30, unit=u.deg), SkyCoord(11, 35, unit=u.deg),
+                                SkyCoord(51, 35, unit=u.deg), SkyCoord(15, 70, unit=u.deg)]:
+
+            # Phase rotating back should not make a difference
+            original_vis = self.vismodel.vis
+            original_uvw = self.vismodel.uvw
+            rotatedvis = phaserotate_visibility(
+                             phaserotate_visibility(self.vismodel, newphasecentre),
+                             self.phasecentre)
+            assert_allclose(rotatedvis.uvw, original_uvw, rtol=1e-11)
+            assert_allclose(rotatedvis.vis, original_vis, rtol=1e-11)
+
+    @unittest.skip("disabled until we've had another good look at create_visibility")
     def test_phase_rotation(self):
-        # Phase rotating back should not make a difference
-        original_vis = self.vismodel.vis.copy()
-        original_uvw = self.vismodel.uvw.copy()
+
+        # Predict visibilities with new phase centre independently
+        vispred = create_visibility(self.vlaa, self.times, self.frequency,
+                                    weight=1.0, phasecentre=self.compabsdirection,
+                                    params=self.params)
+        vismodel2 = predict_visibility(vispred, self.m31sm, self.params)
+
+        # Should yield the same results as rotation
         rotatedvis = phaserotate_visibility(self.vismodel, self.compabsdirection)
-        rotatedvis2 = phaserotate_visibility(self.vismodel, self.phasecentre)
-        assert_allclose(rotatedvis2.uvw, original_uvw, rtol=1e-8)
-        assert_allclose(rotatedvis2.vis, original_vis, rtol=1e-2)
+        assert_allclose(rotatedvis.uvw, vismodel2.uvw, rtol=1e-8)
+        assert_allclose(rotatedvis.vis, vismodel2.vis, rtol=1e-2)
 
 if __name__ == '__main__':
     import sys
