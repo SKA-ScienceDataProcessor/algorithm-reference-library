bf457848dd19bb391e5958e845bc82097b65cccc
timcornwell <realtimcornwell@gmail.com>
2016-09-22 14:12:28 +0100

    parameter test changed slightly
    parmaeters can be nested

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -14,8 +14,8 @@ log = logging.getLogger("arl.test_parameters")
 class TestParameters(unittest.TestCase):
     def setUp(self):
         chome = os.environ['CROCODILE']
-        self.paramsfile = "%s/data/parameters/TestParameters.txt" % chome
-        self.parameters = {'npixel': 256, 'cellsize':0.1, 'predict.cellsize':0.2, 'invert.spectral_mode':'mfs'}
+        self.paramsfile = "%s/tests/TestParameters.txt" % chome
+        self.parameters = {'npixel': 256, 'cellsize':0.1, 'predict':{'cellsize':0.2}, 'invert':{'spectral_mode':'mfs'}}
 
     def test_exportimport(self):
         log_parameters(self.parameters)
@@ -28,10 +28,11 @@ class TestParameters(unittest.TestCase):
     def test_getparameter(self):
     
         assert get_parameter(self.parameters, 'cellsize') == 0.1
-        assert get_parameter(self.parameters, 'predict.cellsize') == 0.2
+        assert get_parameter(self.parameters['predict'], 'cellsize') == 0.2
         assert get_parameter(self.parameters, 'spectral_mode', 'channels') == 'channels'
-        assert get_parameter(self.parameters, 'invert.spectral_mode') == 'mfs'
-        assert get_parameter(self.parameters, 'predict.cellsize') == 0.2
+        assert get_parameter(self.parameters['invert'], 'spectral_mode') == 'mfs'
+        assert get_parameter(self.parameters, 'foo', 'bar') == 'bar'
+        assert get_parameter(self.parameters, 'foo') == None
 
 if __name__ == '__main__':
     unittest.main()
