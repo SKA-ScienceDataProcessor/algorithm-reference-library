72b40abe88cff38326aef20b431f6565bab1cb1c
timcornwell <realtimcornwell@gmail.com>
2016-11-16 21:05:15 +0000

    Setup update
    Needs update to algorithm-reference-library

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 import os
 
-setup(name='crocodile',
+setup(name='algorithm-reference-library',
       version='0.5',
       description='Algorithm Reference Library for Radio Interferometry',
       long_description=open('README.md').read(),
       author='Tim Cornwell, Peter Wortmann, Bojan Nikolic',
       author_email='realtimcornwell@gmail.com',
-      url='https://github.com/SKA-ScienceDataProcessor/crocodile',
+      url='https://github.com/SKA-ScienceDataProcessor/algorithm-reference-library',
       license='Apache License Version 2.0',
       packages=['arl', 'examples', 'tests'],
       test_suite="tests",
