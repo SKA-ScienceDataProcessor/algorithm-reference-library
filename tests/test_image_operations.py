a136cf7e8b1081ac48c96bfa2b16a29b15d1073b
Piers Harding <piers@catalyst.net.nz>
2017-09-25 11:54:19 +1300

    TSK-1823: suppress tests under Jenkins/Travis

--- tests/test_image_operations.py
+++ tests/test_image_operations.py
@@ -21,16 +21,16 @@ log = logging.getLogger(__name__)
 class TestImage(unittest.TestCase):
 
     def setUp(self):
-    
+
         self.dir = './test_results'
         os.makedirs(self.dir, exist_ok=True)
-    
+
         self.m31image = create_test_image(cellsize=0.0001)
         self.cellsize = 180.0 * 0.0001 / numpy.pi
 
     def test_create_image_from_array(self):
         m31model_by_array = create_image_from_array(self.m31image.data, wcs=None)
-        
+
         m31model_by_array = create_image_from_array(self.m31image.data, self.m31image.wcs)
         m31modelsum = add_image(self.m31image, m31model_by_array)
         m31modelsum = add_image(self.m31image, m31model_by_array, docheckwcs=True)
@@ -44,7 +44,7 @@ class TestImage(unittest.TestCase):
         assert numpy.max(numpy.abs(emptyimage.data)) == 0.0
 
     def test_checkwcs(self):
-    
+
         newwcs = self.m31image.wcs.deepcopy()
         newwcs = self.m31image.wcs
         checkwcs(self.m31image.wcs, newwcs)
@@ -53,15 +53,15 @@ class TestImage(unittest.TestCase):
         newwcs.wcs.cdelt[1] = +cellsize
         # with self.assertRaises(AssertionError):
         #     checkwcs(self.m31image.wcs, newwcs)
-    
+
     def test_reproject(self):
         # Reproject an image
-    
+
         cellsize = 1.5 * self.cellsize
         newwcs = self.m31image.wcs.deepcopy()
         newwcs.wcs.cdelt[0] = -cellsize
         newwcs.wcs.cdelt[1] = +cellsize
-    
+
         newshape = numpy.array(self.m31image.data.shape)
         newshape[2] /= 1.5
         newshape[3] /= 1.5
@@ -82,14 +82,14 @@ class TestImage(unittest.TestCase):
             assert polimage.data.dtype == 'complex'
             assert rstokes.data.dtype == 'complex'
             numpy.testing.assert_array_almost_equal(stokes.data, rstokes.data.real, 12)
-            
+
     def test_polarisation_frame_from_wcs(self):
         assert self.m31image.polarisation_frame == PolarisationFrame("stokesI")
         stokes = create_test_image(cellsize=0.0001, polarisation_frame=PolarisationFrame("stokesIQUV"))
         wcs = stokes.wcs.deepcopy()
         shape = stokes.shape
         assert polarisation_frame_from_wcs(wcs, shape) == PolarisationFrame("stokesIQUV")
-        
+
         wcs = stokes.wcs.deepcopy().sub(['stokes'])
         wcs.wcs.crpix[0] = 1.0
         wcs.wcs.crval[0] = -1.0
@@ -112,11 +112,10 @@ class TestImage(unittest.TestCase):
             wcs.wcs.cdelt[0] = -1.0
             pf = polarisation_frame_from_wcs(wcs, shape)
 
-
     def test_smooth_image(self):
         smooth = smooth_image(self.m31image)
         assert numpy.max(smooth.data) > numpy.max(self.m31image.data)
-        
+
     def test_calculate_image_frequency_moments(self):
         frequency = numpy.linspace(0.9e8, 1.1e8, 9)
         cube=create_low_test_image_from_gleam(npixel=512, cellsize=0.0001, frequency=frequency)
@@ -165,16 +164,16 @@ class TestImage(unittest.TestCase):
         m31image = create_test_image(cellsize=0.001, frequency=[1e8], canonical=True)
         padded = pad_image(m31image, [1, 1, 1024, 1024])
         assert padded.shape == (1, 1, 1024, 1024)
-        
+
         padded = pad_image(m31image, [3, 4, 2048, 2048])
         assert padded.shape == (3, 4, 2048, 2048)
-        
+
         with self.assertRaises(ValueError):
             padded = pad_image(m31image, [1, 1, 100, 100])
-            
+
         with self.assertRaises(IndexError):
             padded = pad_image(m31image, [1, 1])
-            
+
     def test_convert_image_to_kernel(self):
         m31image = create_test_image(cellsize=0.001, frequency=[1e8], canonical=True)
         screen = create_w_term_like(m31image, w=20000.0, remove_shift=True)
