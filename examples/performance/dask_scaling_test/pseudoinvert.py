9dc5995b30720f7f328ed5854e45839c74db63bf
timcornwell <realtimcornwell@gmail.com>
2017-11-08 21:09:11 +0000

    Added command line arguments

--- examples/performance/dask_scaling_test/pseudoinvert.py
+++ examples/performance/dask_scaling_test/pseudoinvert.py
@@ -9,7 +9,7 @@ import csv
 
 import numpy
 from dask import delayed
-from distributed import Client, wait, Scheduler
+from distributed import Client, wait
 
 
 # Make some randomly located points on 2D plane
@@ -33,7 +33,6 @@ def write_results(filename, fieldnames, results):
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=',', quotechar='|',
                                 quoting=csv.QUOTE_MINIMAL)
         writer.writerow(results)
-        print(results)
         csvfile.close()
 
 
@@ -49,41 +48,60 @@ if __name__ == '__main__':
     import time
     import socket
     import seqfile
+    import argparse
 
-    import sys
+    parser = argparse.ArgumentParser(description='Benchmark a pseudoinvert in numpy and dask')
+    parser.add_argument('--scheduler', type=str, default=None,
+                        help='The address of a Dask scheduler e.g. 127.0.0.1:8786. If none given one will be created')
+    parser.add_argument('--decimate', type=int, default=1024, help='Decimation in gridding')
+    parser.add_argument('--npixel', type=int, default=1024, help='Number of pixels on a side')
+    parser.add_argument('--nchunks', type=int, default=256, help='Number of chunks of visibilities')
+    parser.add_argument('--len_chunk', type=int, default=16384,
+                        help='Number of visibilities in a chunk = len_chunk * decimate')
+    parser.add_argument('--reduce', type=int, default=16,
+                        help='Gather this number of images in the first stage of the two stage summing images')
+    parser.add_argument('--nnodes', type=int, default=1, help='Number of nodes')
+
+    args = parser.parse_args()
 
     results = {}
-    if len(sys.argv) > 1:
-        client = Client(sys.argv[1])
+    
+    # Process nchunks each of length len_chunk 2d points, making a psf of size shape
+    decimate = args.decimate
+    results['decimate'] = decimate
+    len_chunk = args.len_chunk * decimate
+    results['len_chunk'] = len_chunk
+    nchunks = args.nchunks
+    results['nchunks'] = nchunks
+    nreduce = args.reduce
+    results['reduce'] = nreduce
+    npixel = args.npixel
+    shape = [npixel, npixel]
+    results['npixel'] = npixel
+
+    if args.scheduler is not None:
+        client = Client(args.scheduler)
     else:
         client = Client()
         
     time.sleep(5)
     nworkers = len(client.scheduler_info()['workers'])
-    print("Using %d workers" % (nworkers))
 
     results['nworkers'] = nworkers
+    results['nnodes'] = args.nnodes
     results['hostname'] = socket.gethostname()
     results['epoch'] = time.strftime("%Y-%m-%d %H:%M:%S")
     
-    start = time.time()
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
+    print("Initial state")
+    pp.pprint(results)
+    
     filename = seqfile.findNextFile(prefix='pseudoinvert_%s_' % results['hostname'], suffix='.csv')
     print('Saving results to %s' % filename)
     
-    # Process nchunks each of length len_chunk 2d points, making a psf of size shape
-    decimate = 1024
-    results['decimate'] = decimate
-    len_chunk = 16384 * decimate
-    results['len_chunk'] = len_chunk
-    nchunks = 256
-    results['nchunks'] = nchunks
-    nreduce = 16
-    results['reduce'] = nreduce
-    npixel = 4096
-    shape = [npixel, npixel]
-    results['npixel'] = npixel
     
-    fieldnames = ['nworkers', 'time sparse', 'time psf', 'time sum psf rank1', 'time sum psf',
+    fieldnames = ['nnodes', 'nworkers', 'time sparse', 'time psf', 'time sum psf rank1', 'time sum psf',
                   'max psf', 'npixel', 'len_chunk', 'nchunks', 'reduce', 'decimate',
                   'hostname', 'epoch']
         
@@ -100,10 +118,6 @@ if __name__ == '__main__':
         start = time.time()
         future = client.compute(graph)
         wait(future)
-        # if isinstance(future, list):
-        #     value = [f.result() for f in future]
-        # else:
-        #     value = future.result()
         results['time %s' % name] = time.time() - start
         
         if name == 'sum psf':
@@ -113,5 +127,7 @@ if __name__ == '__main__':
     write_header(filename, fieldnames)
     write_results(filename, fieldnames, results)
     client.shutdown()
-    
+    print("Final state")
+    pp.pprint(results)
+
     exit()
