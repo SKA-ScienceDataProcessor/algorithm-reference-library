2c3f57b1a0f8a631f8af80bb1e5f39a4d9d7605a
IBeArjen <at748@cam.ac.uk>
2017-11-27 15:22:15 +0000

    Modified ffi_demo makefile to fix compilation on some (py3.5?) systems, fix warnings

--- examples/ffi_demo/setup.py
+++ examples/ffi_demo/setup.py
@@ -3,6 +3,11 @@ from distutils.sysconfig import get_config_var
 from setuptools.command.build_ext import build_ext
 from subprocess import call
 import os
+import sys
+
+# Bail on Python < 3
+assert sys.version_info[0] >= 3
+
 
 ffi_demo = Extension('ffi_demo', sources = ['src/ffi_demo.c'])
 
@@ -12,14 +17,14 @@ class CFFIBuild(build_ext):
 
             cmd = [
                     "make",
-                    "BUILD_LIB={}".format(self.build_lib),
-                    "EXT_SUFFIX={}".format(get_config_var('EXT_SUFFIX'))
+                    "BUILD_LIB={}".format(self.build_lib)
                 ]
 
             call(cmd)
 
 setup(name='FFI_Demo',
     version='0.1',
+    python_requires='>=3',
     description = "Demo for compilation of FFI-wrapped Python callable from C",
     ext_modules = [ffi_demo],
     cmdclass = {'build_ext': CFFIBuild})
