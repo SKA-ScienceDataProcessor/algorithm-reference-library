825edbf18125366b7aed3fc8b00c8299e6f10bd3
IBeArjen <at748@cam.ac.uk>
2017-12-04 11:09:07 +0000

    Fix segfault due to missing memory allocations in ffi_demo.c

--- examples/ffi_demo/src/ffi_demo.c
+++ examples/ffi_demo/src/ffi_demo.c
@@ -26,6 +26,8 @@ void *get_ffi_fn_addr(const char* module, const char* fn_name)
 	return (void*)PyNumber_AsSsize_t(fn_addr, NULL);
 }
 
+/* DO NOT USE - we do not want PyObjects */
+/* Leaving for reference only */
 PyObject *get_plain_fn_addr(const char* module, const char* fn_name)
 {
 	PyObject *mod, *fn;
@@ -39,31 +41,46 @@ PyObject *get_plain_fn_addr(const char* module, const char* fn_name)
 
 int main(int argc, char **argv)
 {
-	int i, count;
 	Py_Initialize();
 
+	/* Old demo code using above routines */
+	/*
+	int i, count;
+	
 	int (*pfun)(char *) = get_ffi_fn_addr("ffi_pyroutines", "ffi_phfp");
-	int len = pfun("C-based interface user");
-	printf("Name length was: %d\n", len);
+  int len = pfun("C-based interface user");
+  printf("Name length was: %d\n", len);
 
-	int (*cfun)(void) = get_ffi_fn_addr("ffi_pyroutines", "ffi_gcfp");
-	for(i = 0; i < 10; i++) {
-		count = cfun();
-		printf("\t%d\n", count);
+  int (*cfun)(void) = get_ffi_fn_addr("ffi_pyroutines", "ffi_gcfp");
+  for(i = 0; i < 10; i++) {
+    count = cfun();
+    printf("\t%d\n", count);
+  }
+
+  PyObject *arl_fun = get_plain_fn_addr("arl.util.testing_support", "create_named_configuration");
+  PyObject *config = NULL;
+  PyObject *arg_tup = PyTuple_New(1);
+
+  printf("Set tuple result: %d\n", PyTuple_SetItem(arg_tup, 0, PyUnicode_DecodeFSDefault("LOWBD2-CORE")));
+  pycheck(config = PyObject_CallObject(arl_fun, arg_tup));
+  printf("config pointer: %p\n", config);
+	*/
+
+	ARLVis *vin = malloc(104 * sizeof(char));
+	ARLVis *vout = malloc(104 * sizeof(char));
+
+	vin->nvis = 1;
+	vin->npol = 4;
+
+	// malloc to ARLDataVisSize
+	vin->data = malloc(72+(32*vin->npol*vin->nvis) * sizeof(char));
+	vout->data = malloc(72+(32*vin->npol*vin->nvis) * sizeof(char));
+
+	if (!vin->data || !vout->data) {
+		fprintf(stderr, "Malloc error\n");
+		exit(1);
 	}
 
-	PyObject *arl_fun = get_plain_fn_addr("arl.util.testing_support", "create_named_configuration");
-	PyObject *config = NULL;
-	PyObject *arg_tup = PyTuple_New(1);
-
-	printf("Set tuple result: %d\n", PyTuple_SetItem(arg_tup, 0, PyUnicode_DecodeFSDefault("LOWBD2-CORE")));
-	pycheck(config = PyObject_CallObject(arl_fun, arg_tup));
-	printf("config pointer: %p\n", config);
-
-	ARLVis *vin=malloc(104);
-	vin->nvis=1;
-	vin->npol=4;
-	void *vout=malloc(104);
 	arl_copy_visibility(vin, vout, false);
 
 	return 0;
