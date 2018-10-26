98962464eadd60bf2a66cbd5755dee83d5c498e7
mfarrera <mf582@cam.ac.uk>
2018-10-26 21:44:47 +0100

    Parallel deconvolve

--- workflows/mpi/imaging/imaging_mpi.py
+++ workflows/mpi/imaging/imaging_mpi.py
@@ -27,6 +27,7 @@ from workflows.shared.imaging.imaging_shared import sum_invert_results, remove_s
     threshold_list
 
 from mpi4py import MPI
+log = logging.getLogger(__name__)
 
 def predict_list_mpi_workflow(vis_list, model_imagelist, vis_slices=1,
                                  facets=1, context='2d', comm=MPI.COMM_WORLD, **kwargs):
@@ -144,8 +145,9 @@ def invert_list_mpi_workflow(vis_list, template_model_imagelist, dopsf=False, no
                                 comm=MPI.COMM_WORLD, **kwargs):
     """ Sum results from invert, iterating over the scattered image and vis_list
 
-    :param vis_list:
-    :param template_model_imagelist: Model used to determine image parameters
+    :param vis_list: Only full for rank==0
+    :param template_model_imagelist: Model used to determine image parameters,
+    only valid for rank==0 
     :param dopsf: Make the PSF instead of the dirty image
     :param facets: Number of facets
     :param normalize: Normalize by sumwt
@@ -246,4 +248,188 @@ def invert_list_mpi_workflow(vis_list, template_model_imagelist, dopsf=False, no
 
     return results_vislist
 
+def residual_list_mpi_workflow(vis, model_imagelist, context='2d',comm=MPI.COMM_WORLD, **kwargs):
+    """ Create a graph to calculate residual image using w stacking and faceting
+
+    :param context:
+    :param vis:
+    :param model_imagelist: Model used to determine image parameters
+    :param vis:
+    :param model_imagelist: Model used to determine image parameters
+    :param kwargs: Parameters for functions in components
+    :return:
+    """
+    model_vis = zero_list_serial_workflow(vis)
+    model_vis = predict_list_serial_workflow(model_vis, model_imagelist, context=context, **kwargs)
+    residual_vis = subtract_list_serial_workflow(vis, model_vis)
+    return invert_list_serial_workflow(residual_vis, model_imagelist, dopsf=False, normalize=True, context=context,
+                                       **kwargs)
+
+
+def restore_list_mpi_workflow(model_imagelist, psf_imagelist,
+                              residual_imagelist,comm=MPI.COMM_WORLD, **kwargs):
+    """ Create a graph to calculate the restored image
+
+    :param model_imagelist: Model list
+    :param psf_imagelist: PSF list
+    :param residual_imagelist: Residual list
+    :param kwargs: Parameters for functions in components
+    :return:
+    """
+    return [restore_cube(model_imagelist[i], psf_imagelist[i][0],
+                         residual_imagelist[i][0], **kwargs)
+            for i, _ in enumerate(model_imagelist)]
+
+
+def deconvolve_list_mpi_workflow(dirty_list, psf_list, model_imagelist,
+                                 prefix='', comm=MPI.COMM_WORLD, **kwargs):
+    """Create a graph for deconvolution, adding to the model
+
+    :param dirty_list: only valid in rank==0
+    :param psf_list: only valid in rank==0
+    :param model_imagelist: only valid in rank==0
+    :param kwargs: Parameters for functions in components, valid in all ranks
+    :param comm: MPI communicator
+    :return: (graph for the deconvolution, graph for the flat)
+    """
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    
+    nchan = len(dirty_list)
+    
+    def deconvolve(dirty, psf, model, facet, gthreshold):
+        import time
+        starttime = time.time()
+        if prefix == '':
+            lprefix = "facet %d" % facet
+        else:
+            lprefix = "%s, facet %d" % (prefix, facet)
+        
+        nmoments = get_parameter(kwargs, "nmoments", 0)
+        
+        if nmoments > 0:
+            moment0 = calculate_image_frequency_moments(dirty)
+            this_peak = numpy.max(numpy.abs(moment0.data[0, ...])) / dirty.data.shape[0]
+        else:
+            this_peak = numpy.max(numpy.abs(dirty.data[0, ...]))
+        
+        if this_peak > 1.1 * gthreshold:
+            log.info(
+                "deconvolve_list_serial_workflow %s: cleaning - peak %.6f > 1.1 * threshold %.6f" % (lprefix, this_peak,
+                                                                                                gthreshold))
+            kwargs['threshold'] = gthreshold
+            result, _ = deconvolve_cube(dirty, psf, prefix=lprefix, **kwargs)
+            
+            if result.data.shape[0] == model.data.shape[0]:
+                result.data += model.data
+            else:
+                log.warning(
+                    "deconvolve_list_serial_workflow %s: Initial model %s and clean result %s do not have the same shape" %
+                    (lprefix, str(model.data.shape[0]), str(result.data.shape[0])))
+            
+            flux = numpy.sum(result.data[0, 0, ...])
+            log.info('### %s, %.6f, %.6f, True, %.3f # cycle, facet, peak, cleaned flux, clean, time?'
+                     % (lprefix, this_peak, flux, time.time() - starttime))
+            
+            return result
+        else:
+            log.info("deconvolve_list_serial_workflow %s: Not cleaning - peak %.6f <= 1.1 * threshold %.6f" % (
+                lprefix, this_peak,
+                gthreshold))
+            log.info('### %s, %.6f, %.6f, False, %.3f # cycle, facet, peak, cleaned flux, clean, time?'
+                     % (lprefix, this_peak, 0.0, time.time() - starttime))
+            
+            return copy_image(model)
+    # NOTE: We assume that all processes are called with these parameters
+    # set to the appropriate value, if this cannot be assumed we need to bcast
+    # them
+    deconvolve_facets = get_parameter(kwargs, 'deconvolve_facets', 1)
+    deconvolve_overlap = get_parameter(kwargs, 'deconvolve_overlap', 0)
+    deconvolve_taper = get_parameter(kwargs, 'deconvolve_taper', None)
+    if deconvolve_overlap > 0:
+        deconvolve_number_facets = (deconvolve_facets - 2) ** 2
+    else:
+        deconvolve_number_facets = deconvolve_facets ** 2
+    
+    if rank==0:
+        model_imagelist = image_gather_channels(model_imagelist)
+    
+    # Scatter the separate channel images into deconvolve facets and then gather channels for each facet.
+    # This avoids constructing the entire spectral cube.
+    #    dirty_list = remove_sumwt, nout=nchan)(dirty_list)
+    
+
+        scattered_channels_facets_dirty_list = \
+            [image_scatter_facets(d[0], facets=deconvolve_facets,
+                              overlap=deconvolve_overlap,
+                              taper=deconvolve_taper)
+            for d in dirty_list]
+    
+        # Now we do a transpose and gather
+        scattered_facets_list = [
+            image_gather_channels([scattered_channels_facets_dirty_list[chan][facet]
+                               for chan in range(nchan)])
+            for facet in range(deconvolve_number_facets)]
+    
+        psf_list = remove_sumwt(psf_list)
+        psf_list = image_gather_channels(psf_list)
+    
+        scattered_model_imagelist = \
+            image_scatter_facets(model_imagelist,
+                             facets=deconvolve_facets,
+                             overlap=deconvolve_overlap)
+    
+    # Work out the threshold. Need to find global peak over all dirty_list images
+    threshold = get_parameter(kwargs, "threshold", 0.0)
+    fractional_threshold = get_parameter(kwargs, "fractional_threshold", 0.1)
+    nmoments = get_parameter(kwargs, "nmoments", 0)
+    use_moment0 = nmoments > 0
+    
+    # Find the global threshold. This uses the peak in the average on the frequency axis since we
+    # want to use it in a stopping criterion in a moment clean
+    if rank==0:
+        global_threshold = threshold_list(scattered_facets_list, threshold, fractional_threshold, use_moment0=use_moment0,
+                                      prefix=prefix)
+    
+        facet_list = numpy.arange(deconvolve_number_facets).astype('int')
+    else:
+        scattered_facets_list=list()
+        scattered_model_imagelist=list()
+        facet_list=list()
+        global_threshold=None
+
+    global_threshold=comm.bcast(global_threshold,root=0)
+    # NOTE: check if the three iterables contain the same number of elems
+    # if so, split them and scatter them 
+    assert len(scattered_facets_list) == len(scattered_model_imagelist) ==len(facet_list), "Lists must be the same length"
+    
+    sub_scattered_facets_list=numpy.array_split(scattered_facets_list,size)
+    sub_scattered_facets_list=comm.scatter(sub_scattered_facets_list,root=0)
+    sub_scattered_model_imagelist=numpy.array_split(scattered_model_imagelist,size)
+    sub_scattered_model_imagelist=comm.scatter(sub_scattered_model_imagelist,root=0)
+    sub_facet_list=numpy.array_split(facet_list,size)
+    sub_facet_list=comm.scatter(sub_facet_list,root=0)
+    psf_list=comm.bcast(psf_list,root=0)
+
+    sub_scattered_results_list = [
+        deconvolve(d, psf_list, m, facet, global_threshold)
+        for d, m, facet in zip(sub_scattered_facets_list,
+                               sub_scattered_model_imagelist, sub_facet_list)]
+    scattered_results_list=comm.gather(sub_scattered_results_list)
+    if rank==0:
+        # Gather results from different procs
+        scattered_results_list=numpy.concatenate(scattered_results_list)  
+    # Gather the results back into one image, correcting for overlaps as necessary. The taper function is is used to
+    # feather the facets together
+        gathered_results_list = image_gather_facets(scattered_results_list, model_imagelist,
+                                                facets=deconvolve_facets,
+                                                overlap=deconvolve_overlap,
+                                                taper=deconvolve_taper)
+        flat_list = image_gather_facets(scattered_results_list, model_imagelist,
+                                    facets=deconvolve_facets, overlap=deconvolve_overlap,
+                                    taper=deconvolve_taper, return_flat=True)
+        result=image_scatter_channels(gathered_results_list, subimages=nchan), flat_list
+    else:
+        result=list(),list()
+    return result
 
