a706ee352e8b54d35b009704bd9937cb17097357
timcornwell <realtimcornwell@gmail.com>
2018-12-13 20:36:15 +0000

    Remove remnants of previous modelpartition

--- processing_components/skymodel/operations.py
+++ processing_components/skymodel/operations.py
@@ -10,7 +10,6 @@ from data_models.memory_data_models import SkyModel
 from processing_library.image.operations import copy_image
 from ..skycomponent.base import copy_skycomponent
 from ..skycomponent.operations import filter_skycomponents_by_flux, insert_skycomponent
-from ..visibility.visibility_fitting import fit_visibility
 
 log = logging.getLogger(__name__)
 
@@ -41,32 +40,3 @@ def partition_skymodel_by_flux(sc, model, flux_threshold=-numpy.inf):
     return SkyModel(components=[copy_skycomponent(comp) for comp in brightsc],
                     images=[copy_image(im)],
                     fixed=False)
-
-
-def solve_skymodel(vis, skymodel, gain=0.1, **kwargs):
-    """Fit a single skymodel to a visibility
-    
-    :param evis: Expected vis for this ssm
-    :param modelpartition: scm element being fit i.e. (skymodel, gaintable) tuple
-    :param gain: Gain in step
-    :param method: 'fit' or 'sum'
-    :param kwargs:
-    :return: skycomponent
-    """
-    if skymodel.fixed:
-        return skymodel
-    
-    new_comps = list()
-    for comp in skymodel.components:
-        new_comp = copy_skycomponent(comp)
-        new_comp, _ = fit_visibility(vis, new_comp)
-        new_comp.flux = gain * new_comp.flux + (1.0 - gain) * comp.flux
-        new_comps.append(new_comp)
-    
-    new_images = list()
-    for im in skymodel.images:
-        new_image = copy_image(im)
-        #        new_image = solve_image_arlexecute_workflow(vis, new_image, **kwargs)
-        new_images.append(new_image)
-    
-    return SkyModel(components=new_comps, images=new_images)
