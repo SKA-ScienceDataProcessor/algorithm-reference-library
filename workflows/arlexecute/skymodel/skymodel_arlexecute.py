ebd0d5254fb60fe6098b63817ebb37719648a38c
timcornwell <realtimcornwell@gmail.com>
2018-12-26 12:03:09 +0000

    Optimise skymodel predict and invert

--- workflows/arlexecute/skymodel/skymodel_arlexecute.py
+++ workflows/arlexecute/skymodel/skymodel_arlexecute.py
@@ -2,13 +2,14 @@ import logging
 
 import numpy
 
-from data_models.memory_data_models import Image, GainTable, Visibility, BlockVisibility
-from workflows.arlexecute.imaging.imaging_arlexecute import zero_list_arlexecute_workflow
+from data_models.memory_data_models import Image, GainTable, Visibility
+from processing_library.image.operations import copy_image
 from workflows.serial.imaging.imaging_serial import predict_list_serial_workflow, invert_list_serial_workflow
 from wrappers.arlexecute.calibration.operations import apply_gaintable
 from wrappers.arlexecute.execution_support.arlexecute import arlexecute
 from wrappers.arlexecute.imaging.base import predict_skycomponent_visibility
-from wrappers.arlexecute.visibility.base import copy_visibility
+from wrappers.arlexecute.skycomponent.base import copy_skycomponent
+from wrappers.arlexecute.skycomponent.operations import apply_beam_to_skycomponent
 from wrappers.arlexecute.visibility.coalesce import convert_blockvisibility_to_visibility, \
     convert_visibility_to_blockvisibility
 
@@ -35,51 +36,40 @@ def predict_skymodel_list_arlexecute_workflow(vis_list, skymodel_list, context, 
     
     assert len(vis_list) == len(skymodel_list)
     
-    def dft_cal_sm(v, sm):
+    def ft_cal_sm(v, sm):
         assert isinstance(v, Visibility), v
+        
+        v.data['vis'][...] = 0.0 + 0.0j
+        
         if len(sm.components) > 0:
-            v = predict_skycomponent_visibility(v, sm.components)
-            if docal and isinstance(sm.gaintable, GainTable):
-                bv = convert_visibility_to_blockvisibility(v)
-                bv = apply_gaintable(bv, sm.gaintable)
-                v = convert_blockvisibility_to_visibility(bv)
-        else:
-            v.data['vis'][...] = 0.0 + 0.0j
-        return v
-    
-    def fft_cal_sm(v, sm):
-        assert isinstance(v, Visibility), v
+            
+            if isinstance(sm.mask, Image):
+                comps = copy_skycomponent(sm.components)
+                comps = apply_beam_to_skycomponent(comps, sm.mask)
+                v = predict_skycomponent_visibility(v, comps)
+            else:
+                v = predict_skycomponent_visibility(v, sm.components)
+        
         if isinstance(sm.image, Image):
             if numpy.max(numpy.abs(sm.image.data)) > 0.0:
-                v = predict_list_serial_workflow([v], [sm.image], context=context,
+                if isinstance(sm.mask, Image):
+                    model = copy_image(sm.model)
+                    model.data *= sm.mask.data
+                else:
+                    model = sm.model
+                v = predict_list_serial_workflow([v], [model], context=context,
                                                  vis_slices=vis_slices, facets=facets, gcfcf=gcfcf,
                                                  **kwargs)[0]
-                if docal and isinstance(sm.gaintable, GainTable):
-                    bv = convert_visibility_to_blockvisibility(v)
-                    bv = apply_gaintable(bv, sm.gaintable)
-                    v = convert_blockvisibility_to_visibility(bv)
-            else:
-                v.data['vis'][...] = 0.0 + 0.0j
         
-        else:
-            v.data['vis'][...] = 0.0 + 0.0j
+        if docal and isinstance(sm.gaintable, GainTable):
+            bv = convert_visibility_to_blockvisibility(v)
+            bv = apply_gaintable(bv, sm.gaintable)
+            v = convert_blockvisibility_to_visibility(bv)
+        
         return v
     
-    dft_vis_list = zero_list_arlexecute_workflow(vis_list)
-    dft_vis_list = [arlexecute.execute(dft_cal_sm, nout=1)(dft_vis_list[i], skymodel_list[i])
-                    for i, _ in enumerate(dft_vis_list)]
-    
-    fft_vis_list = zero_list_arlexecute_workflow(vis_list)
-    fft_vis_list = [arlexecute.execute(fft_cal_sm)(fft_vis_list[i], skymodel_list[i])
-                    for i, _ in enumerate(fft_vis_list)]
-    
-    def vis_add(v1, v2):
-        vout = copy_visibility(v1)
-        vout.data['vis'] += v2.data['vis']
-        return vout
-    
-    return [arlexecute.execute(vis_add, nout=1)(dft_vis_list[i], fft_vis_list[i])
-            for i, _ in enumerate(dft_vis_list)]
+    return [arlexecute.execute(ft_cal_sm, nout=1)(vis_list[i], skymodel_list[i])
+            for i, _ in enumerate(vis_list)]
 
 
 def invert_skymodel_list_arlexecute_workflow(vis_list, skymodel_list, context, vis_slices=1, facets=1,
@@ -87,7 +77,7 @@ def invert_skymodel_list_arlexecute_workflow(vis_list, skymodel_list, context, v
     """Calibrate and invert from a skymodel, iterating over the skymodel
 
     The visibility and image are scattered, the visibility is predicted and calibrated on each part, and then the
-    parts are assembled.
+    parts are assembled. The mask if present, is multiplied in at the end.
 
     :param vis_list: List of Visibility data models
     :param skymodel_list: skymodel list
@@ -102,15 +92,20 @@ def invert_skymodel_list_arlexecute_workflow(vis_list, skymodel_list, context, v
     
     assert len(vis_list) == len(skymodel_list)
     
-    def ifft_ical_sm(v, sm):
+    def ift_ical_sm(v, sm):
         assert isinstance(v, Visibility), v
         assert isinstance(sm.image, Image), sm.image
+        
         if docal and isinstance(sm.gaintable, GainTable):
             bv = convert_visibility_to_blockvisibility(v)
             bv = apply_gaintable(bv, sm.gaintable, inverse=True)
             v = convert_blockvisibility_to_visibility(bv)
-        return invert_list_serial_workflow([v], [sm.image], context=context,
-                                           vis_slices=vis_slices, facets=facets, gcfcf=gcfcf,
-                                           **kwargs)[0]
+            
+        result = invert_list_serial_workflow([v], [sm.image], context=context,
+                                             vis_slices=vis_slices, facets=facets, gcfcf=gcfcf,
+                                             **kwargs)[0]
+        if isinstance(sm.mask, Image):
+            result[0].data *= sm.mask
+        return result
     
-    return [arlexecute.execute(ifft_ical_sm)(vis_list[0], sm) for sm in skymodel_list]
+    return [arlexecute.execute(ift_ical_sm)(vis_list[0], sm) for sm in skymodel_list]
