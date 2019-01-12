3a4e5f7c610376b9a09a44ed062fd94f89161ed3
timcornwell <realtimcornwell@gmail.com>
2019-01-12 15:08:38 +0000

    Add missing wrappers for skymodel

--- wrappers/serial/skymodel/operations.py
+++ wrappers/serial/skymodel/operations.py
@@ -2,4 +2,4 @@
 
 """
 from processing_components.skymodel.operations import copy_skymodel, show_skymodel, update_skymodel_from_image, \
-    update_skymodel_from_gaintables
+    update_skymodel_from_gaintables, initialize_skymodel_voronoi
