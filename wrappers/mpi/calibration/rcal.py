3a1668bdcb9f0953e8873af32ff166ed0a21f542
mfarrera <mf582@cam.ac.uk>
2019-03-08 20:44:44 +0000

    wrappers for MPI version

---
+++ wrappers/mpi/calibration/rcal.py
@@ -0,0 +1,4 @@
+""" Real time calibration pipeline
+
+"""
+from processing_components.calibration.rcal import rcal
---
+++ wrappers/mpi/griddata/kernels.py
@@ -0,0 +1,6 @@
+"""
+Functions that define and manipulate kernels
+
+"""
+from processing_components.griddata.kernels import create_pswf_convolutionfunction, \
+    create_awterm_convolutionfunction, create_box_convolutionfunction
