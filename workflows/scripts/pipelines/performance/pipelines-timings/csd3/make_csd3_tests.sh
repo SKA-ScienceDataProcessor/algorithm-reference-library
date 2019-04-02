4fb0061ec5f36c645bbc532f1fe3bceb20778855
timcornwell <realtimcornwell@gmail.com>
2019-04-02 14:04:14 +0100

    Document csd3 scripts

--- workflows/scripts/pipelines/performance/pipelines-timings/csd3/make_csd3_tests.sh
+++ workflows/scripts/pipelines/performance/pipelines-timings/csd3/make_csd3_tests.sh
@@ -10,7 +10,7 @@ for nfreqwin in 64 128 256 512
         filled=`expr ${nfreqwin} / ${nprocs_per_node}`
         for nnodes in ${filled} `expr ${filled} / 2` `expr ${filled} / 4` `expr ${filled} / 8`
             do
-                # NUMBER_NODES NUMBER_PROCS_PER_NODE NUMBER_FREQUENCY_WINDOWS CONTEXT NUMBER_THREADS MEMORY SERIAL
+                # NUMBER_NODES NUMBER_PROCS_PER_NODE NUMBER_FREQUENCY_WINDOWS CONTEXT NUMBER_THREADS MEMORY SERIAL RMAX
                 python make_pipelines-timings.py csd3 ${nnodes} ${nprocs_per_node} ${nfreqwin} ${context} \
                 ${nthreads} ${memory} ${serial} ${rmax}
             done
