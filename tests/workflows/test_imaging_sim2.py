69d9e9cd56f7f366d5a7c2f7c806ef007f351d66
timcornwell <realtimcornwell@gmail.com>
2019-04-14 09:01:18 +0100

    sim2 test detect casa not installed

--- tests/workflows/test_imaging_sim2.py
+++ tests/workflows/test_imaging_sim2.py
@@ -33,36 +33,40 @@ class TestImagingSim2(unittest.TestCase):
         pass
     
     def test_sim2(self):
-        bvt = create_blockvisibility_from_ms(arl_path('data/vis/sim-2.ms'), channum=[35, 36, 37, 38, 39])[0]
-        bvt.configuration.diameter[...] = 35.0
-        vt = convert_blockvisibility_to_visibility(bvt)
-        vt = convert_visibility_to_stokes(vt)
         
-        cellsize = 20.0 * numpy.pi / (180.0 * 3600.0)
-        npixel = 512
-        
-        model = create_image_from_visibility(vt, cellsize=cellsize, npixel=npixel,
-                                             polarisation_frame=PolarisationFrame('stokesIQUV'))
-        dirty, sumwt = invert_list_serial_workflow([vt], [model], context='2d')[0]
-        psf, sumwt = invert_list_serial_workflow([vt], [model], context='2d', dopsf=True)[0]
-        export_image_to_fits(dirty, '%s/imaging_sim2_dirty.fits' % (self.dir))
-        export_image_to_fits(psf, '%s/imaging_sim2_psf.fits' % (self.dir))
-        
-        # Deconvolve using clean
-        comp, residual = deconvolve_cube(dirty, psf, niter=10000, threshold=0.001, fractional_threshold=0.001,
-                                         window_shape='quarter', gain=0.7, scales=[0, 3, 10, 30])
-        
-        restored = restore_cube(comp, psf, residual)
-        export_image_to_fits(restored, '%s/imaging_sim2_restored.fits' % (self.dir))
-        export_image_to_fits(residual, '%s/imaging_sim2_residual.fits' % (self.dir))
-        
-        qa = qa_image(restored)
-        
-        assert numpy.abs(qa.data['max'] - 1.006140596404203) < 1e-7, qa
-        assert numpy.abs(qa.data['maxabs'] - 1.006140596404203) < 1e-7, qa
-        assert numpy.abs(qa.data['min'] + 0.23890808520954754) < 1e-7, qa
-        assert numpy.abs(qa.data['rms'] - 0.007366519782047875) < 1e-7, qa
-        assert numpy.abs(qa.data['medianabs'] - 0.0005590537883509844) < 1e-7, qa
+        try:
+            bvt = create_blockvisibility_from_ms(arl_path('data/vis/sim-2.ms'), channum=[35, 36, 37, 38, 39])[0]
+            bvt.configuration.diameter[...] = 35.0
+            vt = convert_blockvisibility_to_visibility(bvt)
+            vt = convert_visibility_to_stokes(vt)
+            
+            cellsize = 20.0 * numpy.pi / (180.0 * 3600.0)
+            npixel = 512
+            
+            model = create_image_from_visibility(vt, cellsize=cellsize, npixel=npixel,
+                                                 polarisation_frame=PolarisationFrame('stokesIQUV'))
+            dirty, sumwt = invert_list_serial_workflow([vt], [model], context='2d')[0]
+            psf, sumwt = invert_list_serial_workflow([vt], [model], context='2d', dopsf=True)[0]
+            export_image_to_fits(dirty, '%s/imaging_sim2_dirty.fits' % (self.dir))
+            export_image_to_fits(psf, '%s/imaging_sim2_psf.fits' % (self.dir))
+            
+            # Deconvolve using clean
+            comp, residual = deconvolve_cube(dirty, psf, niter=10000, threshold=0.001, fractional_threshold=0.001,
+                                             window_shape='quarter', gain=0.7, scales=[0, 3, 10, 30])
+            
+            restored = restore_cube(comp, psf, residual)
+            export_image_to_fits(restored, '%s/imaging_sim2_restored.fits' % (self.dir))
+            export_image_to_fits(residual, '%s/imaging_sim2_residual.fits' % (self.dir))
+            
+            qa = qa_image(restored)
+            
+            assert numpy.abs(qa.data['max'] - 1.006140596404203) < 1e-7, qa
+            assert numpy.abs(qa.data['maxabs'] - 1.006140596404203) < 1e-7, qa
+            assert numpy.abs(qa.data['min'] + 0.23890808520954754) < 1e-7, qa
+            assert numpy.abs(qa.data['rms'] - 0.007366519782047875) < 1e-7, qa
+            assert numpy.abs(qa.data['medianabs'] - 0.0005590537883509844) < 1e-7, qa
+        except ModuleNotFoundError:
+            pass
 
 
 if __name__ == '__main__':
