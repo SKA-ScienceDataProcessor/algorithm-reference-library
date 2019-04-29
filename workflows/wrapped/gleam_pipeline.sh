5fd7f0c392be0681d2b73376f55e58e3980ec3e9
timcornwell <realtimcornwell@gmail.com>
2019-04-29 16:20:53 +0100

    Remove old json definitions - deadend

--- workflows/wrapped/gleam_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd ${ARL}/workflows/wrapped
-comp_location=${ARL}/processing_components/processing_component_interface
-python ${comp_location}/processing_component_interface.py --config gleam_create_vislist.json
-python ${comp_location}/processing_component_interface.py --config gleam_create_skymodel.json
-python ${comp_location}/processing_component_interface.py --config gleam_predict_vislist.json
-python ${comp_location}/processing_component_interface.py --config gleam_continuum_imaging.json
