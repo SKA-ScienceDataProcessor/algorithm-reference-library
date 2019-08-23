0ae7752f907b879cd7612cdf1c97033b9988b7a0
timcornwell <realtimcornwell@gmail.com>
2019-08-23 11:05:28 +0100

    Slight change in test result due to photutils change

--- tests/workflows/test_pipelines_mpc_arlexecute.py
+++ tests/workflows/test_pipelines_mpc_arlexecute.py
@@ -248,7 +248,7 @@ class TestPipelineMPC(unittest.TestCase):
         recovered_mpccal_components = sorted(recovered_mpccal_components, key=max_flux, reverse=True)
         
         assert recovered_mpccal_components[0].name == 'Segment 5', recovered_mpccal_components[0].name
-        assert numpy.abs(recovered_mpccal_components[0].flux[0, 0] - 7.092364752026981) < 1e-7, \
+        assert numpy.abs(recovered_mpccal_components[0].flux[0, 0] - 7.092364367618823) < 1e-7, \
             recovered_mpccal_components[0].flux[0, 0]
         
         newscreen = create_empty_image_like(self.screen)
